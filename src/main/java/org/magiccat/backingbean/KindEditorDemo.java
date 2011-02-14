dad7a1f2d605f088c462a5d9a1907e902d3d10a0
cleverpig <greatcleverpig@gmail.com>
2011-02-14 16:15:42 +0800

    add:kindeditor component with fileUpload and fileManager features.

---
+++ src/main/java/org/magiccat/backingbean/KindEditorDemo.java
@@ -0,0 +1,56 @@
+package org.magiccat.backingbean;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cleverpig
+ * Date: 11-1-25
+ * Time: 下午2:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class KindEditorDemo {
+  private String editContent;
+  private String htmlContent;
+  private Log log= LogFactory.getLog(KindEditorDemo.class);
+
+  public String getHtmlContent() {
+    return htmlContent;
+  }
+
+  public String getEditContent() {
+    return editContent;
+  }
+
+  public void setEditContent(String editContent) {
+    this.editContent = editContent;
+  }
+
+  public void setHtmlContent(String htmlContent) {
+    this.htmlContent = htmlContent;
+  }
+
+  public void submitActionListener(ActionEvent event){
+    log.debug("editContent:"+editContent);
+    FacesContext context = FacesContext.getCurrentInstance();
+    //TODO:we only can validate form submmitted value is not empty,since kindEditor's require attribute didn't work anyway.
+    if(StringUtils.isEmpty(editContent)){
+      FacesMessage message = new FacesMessage();
+      message.setSeverity(FacesMessage.SEVERITY_ERROR);
+      message.setSummary("editor Field is Blank");
+      message.setDetail("editor Field is Blank..");
+      context.addMessage("form:editor",message);
+    }
+    else{
+      htmlContent=editContent;
+    }
+  }
+
+
+}
---
+++ src/main/webapp/js/kindeditor-config.js
@@ -0,0 +1,29 @@
+/**
+ * Created by .
+ * User: cleverpig
+ * Date: 11-1-25
+ * Time: 下午1:58
+ * To change this template use File | Settings | File Templates.
+ */
+var config=new Object();
+config.height='400px';
+config.width='400px';
+config.skinType='default';
+config.items=['undo', 'redo', 'print', 'cut', 'copy', 'paste',
+  'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
+  'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
+  'superscript', '|', 'selectall', '-',
+  'title', 'fontname', 'fontsize', '|', 'textcolor', 'bgcolor', 'bold',
+  'italic', 'underline', 'strikethrough', 'removeformat', '|', 'image',
+  'flash', 'media', 'advtable', 'hr', 'emoticons', 'link', 'unlink'];
+
+var basic_edit=new Object();
+basic_edit.height='300px';
+basic_edit.width='400px';
+basic_edit.skinType='default';
+basic_edit.items=[
+    'source','undo', 'redo', 'print', 'cut', 'copy', 'paste','plainpaste', 'wordpaste', '|',
+    'justifyleft', 'justifycenter', 'justifyright','justifyfull','|','image','-',
+    'title', 'fontname', 'fontsize','|','textcolor', 'bgcolor', 'bold','italic', 'underline', 'strikethrough', 'removeformat'
+  ];
+
